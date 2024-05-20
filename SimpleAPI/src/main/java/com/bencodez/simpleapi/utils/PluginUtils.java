7ac9cbb4b71ad2793e758d936260fba0a33a9a2c
BenCodez <benbergen12@gmail.com>
2024-05-20 10:04:39 -0400

    Add plugin utils class

---
+++ SimpleAPI/src/main/java/com/bencodez/simpleapi/utils/PluginUtils.java
@@ -0,0 +1,33 @@
+package com.bencodez.simpleapi.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginUtils {
+
+	private PluginUtils() {
+	}
+
+	public static long getFreeMemory() {
+		return Runtime.getRuntime().freeMemory() / (1024 * 1024);
+	}
+
+	public static long getMemory() {
+		return Runtime.getRuntime().totalMemory() / (1024 * 1024);
+	}
+
+	public static void registerCommands(JavaPlugin plugin, String commandText, CommandExecutor executor, TabCompleter tab) {
+		plugin.getCommand(commandText).setExecutor(executor);
+		if (tab != null) {
+			plugin.getCommand(commandText).setTabCompleter(tab);
+		}
+	}
+
+	public static  void registerEvents(Listener listener, JavaPlugin plugin) {
+		Bukkit.getPluginManager().registerEvents(listener, plugin);
+	}
+
+}
