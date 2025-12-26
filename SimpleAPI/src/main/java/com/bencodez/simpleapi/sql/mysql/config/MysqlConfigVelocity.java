50e488cf66eef86fc6151236048f892d9f5add8a
BenCodez <benbergen12@gmail.com>
2025-12-26 09:23:41 -0600

    Refactor MysqlConfigVelocity to use a helper method for node retrieval

--- SimpleAPI/src/main/java/com/bencodez/simpleapi/sql/mysql/config/MysqlConfigVelocity.java
+++ SimpleAPI/src/main/java/com/bencodez/simpleapi/sql/mysql/config/MysqlConfigVelocity.java
@@ -3,6 +3,8 @@ package com.bencodez.simpleapi.sql.mysql.config;
 import com.bencodez.simpleapi.file.velocity.VelocityYMLFile;
 import com.bencodez.simpleapi.sql.mysql.DbType;
 
+import ninja.leaping.configurate.ConfigurationNode;
+
 public class MysqlConfigVelocity extends MysqlConfig {
 
 	public MysqlConfigVelocity(String prePath, VelocityYMLFile config) {
@@ -15,54 +17,65 @@ public class MysqlConfigVelocity extends MysqlConfig {
 
 	private void load(String prePath, VelocityYMLFile config) {
 		// --- Basic Table Info ---
-		setTablePrefix(config.getString(config.getNode(prePath, "Prefix"), ""));
-		String name = config.getString(config.getNode(prePath, "Name"), "");
+		setTablePrefix(config.getString(node(config, prePath, "Prefix"), ""));
+		String name = config.getString(node(config, prePath, "Name"), "");
 		if (name != null && !name.isEmpty()) {
 			setTableName(name);
 		}
 
 		// --- Connection Info ---
-		setHostName(config.getString(config.getNode(prePath, "Host"), ""));
-		setPort(config.getInt(config.getNode(prePath, "Port"), 0));
-		setUser(config.getString(config.getNode(prePath, "Username"), ""));
-		setPass(config.getString(config.getNode(prePath, "Password"), ""));
-		setDatabase(config.getString(config.getNode(prePath, "Database"), ""));
+		setHostName(config.getString(node(config, prePath, "Host"), ""));
+		setPort(config.getInt(node(config, prePath, "Port"), 0));
+		setUser(config.getString(node(config, prePath, "Username"), ""));
+		setPass(config.getString(node(config, prePath, "Password"), ""));
+		setDatabase(config.getString(node(config, prePath, "Database"), ""));
 
 		// --- Pool Settings ---
-		setLifeTime(config.getLong(config.getNode(prePath, "MaxLifeTime"), -1));
-		setMaxThreads(config.getInt(config.getNode(prePath, "MaxConnections"), 1));
+		setLifeTime(config.getLong(node(config, prePath, "MaxLifeTime"), -1));
+		setMaxThreads(config.getInt(node(config, prePath, "MaxConnections"), 1));
 		if (getMaxThreads() < 1) {
 			setMaxThreads(1);
 		}
 
 		// Recommended tuning defaults
-		setMinimumIdle(config.getInt(config.getNode(prePath, "MinimumIdle"), 2)); // 2
-		setIdleTimeoutMs(config.getLong(config.getNode(prePath, "IdleTimeoutMs"), 10 * 60_000L)); // 10 min
-		setKeepaliveMs(config.getLong(config.getNode(prePath, "KeepaliveMs"), 5 * 60_000L)); // 5 min
-		setValidationMs(config.getLong(config.getNode(prePath, "ValidationMs"), 5_000L)); // 5 s
-		setLeakDetectMs(config.getLong(config.getNode(prePath, "LeakDetectMs"), 20_000L)); // 20 s
-		setConnectionTimeout(config.getInt(config.getNode(prePath, "ConnectionTimeout"), 50_000)); // 50 s
+		setMinimumIdle(config.getInt(node(config, prePath, "MinimumIdle"), 2)); // 2
+		setIdleTimeoutMs(config.getLong(node(config, prePath, "IdleTimeoutMs"), 10 * 60_000L)); // 10 min
+		setKeepaliveMs(config.getLong(node(config, prePath, "KeepaliveMs"), 5 * 60_000L)); // 5 min
+		setValidationMs(config.getLong(node(config, prePath, "ValidationMs"), 5_000L)); // 5 s
+		setLeakDetectMs(config.getLong(node(config, prePath, "LeakDetectMs"), 20_000L)); // 20 s
+		setConnectionTimeout(config.getInt(node(config, prePath, "ConnectionTimeout"), 50_000)); // 50 s
 
 		// --- Driver / DB Selection ---
-		String dbTypeStr = config.getString(config.getNode(prePath, "DbType"), "");
+		String dbTypeStr = config.getString(node(config, prePath, "DbType"), "");
 		if (dbTypeStr != null && !dbTypeStr.isEmpty()) {
 			setDbType(DbType.fromString(dbTypeStr));
 		} else {
-			boolean maria = config.getBoolean(config.getNode(prePath, "UseMariaDB"), false);
+			boolean maria = config.getBoolean(node(config, prePath, "UseMariaDB"), false);
 			setDbType(maria ? DbType.MARIADB : DbType.MYSQL);
 		}
 
 		// Optional explicit JDBC driver override
-		setDriver(config.getString(config.getNode(prePath, "Driver"), ""));
+		setDriver(config.getString(node(config, prePath, "Driver"), ""));
 
 		// --- Driver / Behavior Options ---
-		setUseSSL(config.getBoolean(config.getNode(prePath, "UseSSL"), false));
-		setPublicKeyRetrieval(config.getBoolean(config.getNode(prePath, "PublicKeyRetrieval"), false));
-		setUseMariaDB(config.getBoolean(config.getNode(prePath, "UseMariaDB"), false));
+		setUseSSL(config.getBoolean(node(config, prePath, "UseSSL"), false));
+		setPublicKeyRetrieval(config.getBoolean(node(config, prePath, "PublicKeyRetrieval"), false));
+		setUseMariaDB(config.getBoolean(node(config, prePath, "UseMariaDB"), false));
 
 		// --- Additional Settings ---
-		setLine(config.getString(config.getNode(prePath, "Line"), ""));
-		setDebug(config.getBoolean(config.getNode(prePath, "Debug"), false));
-		setPoolName(config.getString(config.getNode(prePath, "PoolName"), ""));
+		setLine(config.getString(node(config, prePath, "Line"), ""));
+		setDebug(config.getBoolean(node(config, prePath, "Debug"), false));
+		setPoolName(config.getString(node(config, prePath, "PoolName"), ""));
+	}
+
+	/**
+	 * Returns a valid config node. - If prePath is empty/null -> root-level key -
+	 * Otherwise -> prePath.key
+	 */
+	private ConfigurationNode node(VelocityYMLFile config, String prePath, String key) {
+		if (prePath == null || prePath.isEmpty()) {
+			return config.getNode(key);
+		}
+		return config.getNode(prePath, key);
 	}
 }
